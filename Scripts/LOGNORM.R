b2765870e8f089e6f3867cadca76b7a83eed8384
MilouArts <70600687+MilouArts@users.noreply.github.com>
2021-09-01 17:49:33 +0200

    Update LOGNORM.R
    created an if loop so it is possible to create two normalized datasets next to eachother (lognorm and TIC normalized)

--- Scripts/LOGNORM.R
+++ Scripts/LOGNORM.R
@@ -9,11 +9,18 @@ Milou Arts, NIOZ, NL, 2019
 List of alterations:
 
 '
-analysis_info$normalization<-"normalized by $log10(peak area)$ transformation"
-analysis_info$normalization_zeroes_replaced_by<-zeroes
+
 setwd(dirOutput)
 
-df1.trans.feat<-df.filtered #create backup of your data, although this should be the same as "rawpeaks_no-background_no-transientfeat.csv"
+#create backup of your data, although this should be the same as "rawpeaks_no-background_no-transientfeat.csv"
+if (!exists("df1.trans.feat")){
+        df1.trans.feat<-df.filtered
+        analysis_info$normalization<-"normalized by $log10(peak area)$ transformation"
+        analysis_info$normalization_zeroes_replaced_by<-zeroes
+} else { df.filtered<-read.csv("rawpeaks_no-background_no-transientfeat.csv", row.names = 1)
+        analysis_info$normalization_2<-"normalized by $log10(peak area)$ transformation"
+        analysis_info$normalization_2_zeroes_replaced_by<-zeroes }
+
 df.filtered<-as.data.frame(t(df.filtered))
 
 #Normalize by log 10 transformation
@@ -30,7 +37,7 @@ LOGNORM<-sapply(df.filtered, FUNLOG)
 LOGNORM<-as.data.frame(LOGNORM)
 rownames(LOGNORM)<-rownames(df.filtered)
 setwd(dirOutput)
-write.csv(LOGNORM,"LOGNORM_raw.csv",row.names = TRUE) 
+write.csv(LOGNORM,"LOGNORM_raw.csv",row.names = TRUE)
 
 #replace the 0s with the number you want. - i couldve done this before log transformation. but now i also have a raw version)
 tmp<-LOGNORM
@@ -43,7 +50,7 @@ tmp[which(LOGNORM == -Inf, arr.ind = TRUE)]<- log10zeroes
 df.norm.area<-tmp
 df.norm.area<-rownames_to_column(df.norm.area, "File Name")
 
-write.csv(df.norm.area,"df.norm.area_no_metadata.csv",row.names = FALSE) 
+write.csv(df.norm.area,"df.norm.area_no_metadata.csv",row.names = FALSE)
 
 analysis_info$nr_replaced_zeroes_lognorm<-length(log10zeroes)
 rm(log10zeroes)
@@ -54,7 +61,7 @@ if (!exists("full_metadata")){
 df.norm.area<-dplyr::right_join(full_metadata, df.norm.area, by = "File Name")
 df.norm.area<-if_na(df.norm.area, "not applicable")
 setwd(dirOutput)
-write.csv(df.norm.area,"df.norm.area.csv",row.names = FALSE) 
+write.csv(df.norm.area,"df.norm.area.csv",row.names = FALSE)
 
 #number of metadata columns
 M<-ncol(full_metadata)+1
